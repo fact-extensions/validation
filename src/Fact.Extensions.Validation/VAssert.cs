5f248c0b959ec47e55eb0e14569c50dfaab1d11d
Malachi Burke <malachi.burke@gmail.com>
2017-01-09 19:34:04 -0800

    Bringing in new freshened VAssert

---
+++ src/Fact.Extensions.Validation/VAssert.cs
@@ -0,0 +1,81 @@
+﻿//using Fact.Extensions.Collection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// TODO: Put this out into Fact.Extensions.Validation
+// refinement of existing VAssert classes
+// they are pretty awesome, but also a bit wily so redoing them
+namespace Fact.Extensions.Validation
+{
+    /// <summary>
+    /// Assertions across a bag of properties
+    /// </summary>
+    public class VAssert
+    {
+    }
+
+
+    /// <summary>
+    /// Assertions for one parameter only
+    /// </summary>
+    public class VAssertSingle
+    {
+        readonly string parameterName;
+        readonly object parameterValue;
+
+        // TODO: bring in proper ErrorItem directly from Apprentice, and call event using
+        // that as a parameter
+        // format for now is: parameter, description, offending value
+        public event Action<string, string, object> ErrorFound;
+
+        public VAssertSingle(string parameterName, object parameterValue)
+        {
+            this.parameterName = parameterName;
+            this.parameterValue = parameterValue;
+        }
+
+        public string Name => parameterName;
+
+        public object Value => parameterValue;
+
+        internal void Error(string description)
+        {
+            ErrorFound?.Invoke(parameterName, description, parameterValue);
+        }
+
+        internal bool IsTrue(bool condition, string failureDescription)
+        {
+            if (!condition)
+            {
+                Error(failureDescription);
+                return false;
+            }
+            return true;
+        }
+    }
+
+
+    public class VAssertSingle<T> : VAssertSingle
+    {
+        new public T Value => (T)base.Value;
+
+        /*
+        public static explicit operator T(VAssertSingle<T> assert)
+        {
+            return assert.Value;
+        }*/
+
+        public static implicit operator T(VAssertSingle<T> assert)
+        {
+            return assert.Value;
+        }
+
+        public VAssertSingle(string parameterName, T parameterValue)
+            : base(parameterName, parameterValue)
+        {
+
+        }
+    }
+}
---
+++ src/Fact.Extensions.Validation/VAssertSingle.Extensions.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fact.Extensions.Validation
+{
+    public static class VAssertSingle_Extensions
+    {
+        public static void IsTrue(this VAssertSingle<bool> assert)
+        {
+            if (!assert.Value) assert.Error("Value expected to be true");
+        }
+
+        public static void IsFalse(this VAssertSingle<bool> assert)
+        {
+            if (assert.Value) assert.Error("Value expected to be false");
+        }
+
+
+        public static bool IsEqual(this VAssertSingle<string> assert, string expected)
+        {
+            return assert.IsTrue(assert.Value.Equals(expected), "Strings must be equal");
+        }
+
+
+        public static VAssertSingle<int> Length(this VAssertSingle<string> assert)
+        {
+            return new VAssertSingle<int>(assert.Name + ".Length", assert.Value.Length);
+        }
+
+
+        public static bool IsUnderLength(this VAssertSingle<string> assert, int maximumLength)
+        {
+            return assert.IsTrue(assert.Value.Length < maximumLength, "String length cannot exceed " + maximumLength);
+        }
+
+
+        public static bool IsLessThan<T>(this VAssertSingle<T> assert, int maximum)
+            where T : IComparable
+        {
+            // TODO: Haven't actually tested this yet
+            return assert.IsTrue(assert.Value.CompareTo(maximum) < 0, "Must be less than " + maximum);
+        }
+
+
+        public static bool IsGreaterThan<T>(this VAssertSingle<T> assert, int maximum)
+            where T : IComparable
+        {
+            // TODO: Haven't actually tested this yet
+            return assert.IsTrue(assert.Value.CompareTo(maximum) > 0, "Must be greater than " + maximum);
+        }
+
+
+        public static bool IsNull<T>(this VAssertSingle<T> assert)
+            where T : class
+        {
+            var result = assert.Value == null;
+
+            if (!result) assert.Error("Cannot be null");
+
+            return result;
+        }
+
+        public static bool IsNotNull<T>(this VAssertSingle<T> assert)
+            where T : class
+        {
+            var result = assert.Value != null;
+
+            if (!result) assert.Error("Cannot be null");
+
+            return result;
+        }
+    }
+}
