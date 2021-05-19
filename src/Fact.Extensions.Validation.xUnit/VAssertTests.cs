1e3680fef149a3380ed8eb094c9ee9f30e789179
Malachi Burke <malachi.burke@gmail.com>
2021-05-19 10:23:09 -0700

    xUnit boilerplate

---
+++ src/Fact.Extensions.Validation.xUnit/VAssertTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Fact.Extensions.Validation.xUnit
+{
+    public class VAssertTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
