ec4fe11118a3c432c4646231eedcc05d0601bcfe
Malachi Burke <malachi.burke@gmail.com>
2021-11-05 09:50:55 -0700

    Cleanup (file didn't get saved from before)

--- src/Fact.Extensions.Validation/Experimental/FluentExtensions.cs
+++ src/Fact.Extensions.Validation/Experimental/FluentExtensions.cs
@@ -204,49 +204,7 @@ namespace Fact.Extensions.Validation.Experimental
         }
 
 
-        /// <summary>
-        /// Configures optional setter to write back to validating source
-        /// </summary>
-        /// <typeparam name="TFluentBinder"></typeparam>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="fluentBinder"></param>
-        /// <param name="setter"></param>
-        /// <param name="initialGetter"></param>
-        /// <returns></returns>
-        /// <remarks>
-        /// Be careful - no compile time enforcement of T
-        /// Mainly so that we can experiment with Win32 Registry setters
-        /// Otherwise this could be IFluentBinder<typeparamref name="T"/> constrained
-        /// </remarks>
-        public static TFluentBinder Setter<TFluentBinder, T>(this TFluentBinder fluentBinder, Action<T> setter,
-            Func<T> initialGetter = null)
-            where TFluentBinder: IFluentBinder
-        {
-            var binder = (IBinderBase<T>)fluentBinder.Binder;
-            binder.setter = setter;
-            if(initialGetter != null)
-                setter(initialGetter());
-            return fluentBinder;
-        }
 
-
-        public static IFluentBinder<T> Commit<T>(this IFluentBinder<T> fluentBinder, Committer committer, Action<T> commit)
-        {
-            committer.Committing += () =>
-            {
-                //commit(fb.InitialValue);
-                // DEBT: We may prefer to use InitialValue, though fb.Field.Value is smart enough to get us the
-                // right thing
-                IField<T> f = fluentBinder.Field;
-                commit(f.Value);
-                return new ValueTask();
-            };
-            return fluentBinder;
-        }
-
-
-        public static IFluentBinder<T> Commit<T>(this IFluentBinder<T> fluentBinder, Action<T> commit) =>
-            fluentBinder.Commit(fluentBinder.Binder.Committer, commit);
 
         
         public static TFluentBinder Required<TFluentBinder, T>(this TFluentBinder fluentBinder, Func<T, bool> isEmpty)
