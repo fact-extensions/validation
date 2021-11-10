e4e7bb2af79eb28110e8814873f91ec0be1c4531
Malachi Burke <malachi.burke@gmail.com>
2021-11-09 21:22:45 -0800

    Experimenting with modular fluent code

---
+++ src/Fact.Extensions.Validation.xUnit/ExperimentalTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Fact.Extensions.Validation.xUnit
+{
+    using Experimental;
+
+    public class ExperimentalTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var mp = new ModuleProvider<Optional<string>, Synthetic.SyntheticEntity1>(
+                new Optional<string>(),
+                new Synthetic.SyntheticEntity1());
+
+            mp.Test1("value");
+        }
+    }
+}
---
+++ src/Fact.Extensions.Validation/Experimental/FluentProvider.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fact.Extensions.Validation.Experimental
+{
+    public interface IModuleProvider<TProvided>
+    {
+        TProvided Provided { get; }
+    }
+
+
+    public class ModuleProvider<TProvided> : 
+        IModuleProvider<TProvided>
+    {
+        readonly TProvided provided1;
+
+        TProvided IModuleProvider<TProvided>.Provided => provided1;
+
+        public ModuleProvider(TProvided provided)
+        {
+            provided1 = provided;
+        }
+    }
+
+    public class ModuleProvider<TProvided1, TProvided2> : ModuleProvider<TProvided1>,
+        IModuleProvider<TProvided2>
+    {
+        readonly TProvided2 provided2;
+
+        TProvided2 IModuleProvider<TProvided2>.Provided => provided2;
+
+        public ModuleProvider(TProvided1 provided1, TProvided2 provided2) : base(provided1)
+        {
+            this.provided2 = provided2;
+        }
+    }
+
+    /*
+    public class ModuleProvider<TProvided1, TProvided2, TProvided3> : ModuleProvider<TProvided1, TProvided2>,
+        IModuleProvider<TProvided3>
+    {
+        TProvided3 IModuleProvider<TProvided3>.Provided { get; }
+    } */
+
+    public static class FluentModuleExtensions
+    {
+        public static TModuleProvider Test1<TModuleProvider, T>(this TModuleProvider test1, T setTo)
+            where TModuleProvider: IModuleProvider<Optional<T>>
+            //where TOptional: Optional<T>
+        {
+            test1.Provided.Value = setTo;
+            return test1;
+        }
+    }
+}
