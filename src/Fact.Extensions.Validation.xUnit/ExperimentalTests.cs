691e1ae2458d568a68304652dbaccd95ac353c69
Malachi <malachi.burke@gmail.com>
2021-11-18 18:25:34 -0800

    Clunky first try at LossyQueue runner

--- src/Fact.Extensions.Validation.xUnit/ExperimentalTests.cs
+++ src/Fact.Extensions.Validation.xUnit/ExperimentalTests.cs
@@ -6,6 +6,8 @@ using Xunit;
 namespace Fact.Extensions.Validation.xUnit
 {
     using Experimental;
+    using FluentAssertions;
+    using System.Threading.Tasks;
 
     public class ExperimentalTests
     {
@@ -21,6 +23,43 @@ namespace Fact.Extensions.Validation.xUnit
 
             mp.Test1("value");
             mp.Get(out assigner);
+        }
+
+
+        [Fact]
+        public async Task LossyQueueTest()
+        {
+            var lq = new LossyQueue();
+            var tcs = new TaskCompletionSource<int>();
+            var tcs2 = new TaskCompletionSource<int>();
+            int val = 0;
+
+            lq.Add(async () =>
+            {
+                val = await tcs.Task;
+            });
+
+            // this middle one should get lost, since the first one is waiting for tcs
+            lq.Add(() =>
+            {
+                val += 1;
+                tcs2.SetResult(val);
+                return new ValueTask();
+            });
+
+            // this one will run, since it's the last entry before tcs.SetResult
+            lq.Add(() =>
+            {
+                val += 100;
+                tcs2.SetResult(val);
+                return new ValueTask();
+            });
+
+            tcs.SetResult(5);
+
+            var val3 = await tcs2.Task;
+
+            val3.Should().Be(105);
         }
     }
 }
---
+++ src/Fact.Extensions.Validation/Experimental/LossyQueue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fact.Extensions.Validation.Experimental
+{
+    public class LossyQueue
+    {
+        Func<ValueTask> queued;
+        Func<ValueTask> current;
+
+        async ValueTask Runner()
+        {
+            await current();
+            if(queued != null)
+            {
+                // to avoid infinite loop
+                var temp = queued;
+                queued = null;
+
+                Current = temp;
+            }
+        }
+
+        Func<ValueTask> Current
+        {
+            get
+            {
+                return current;
+            }
+            set
+            {
+                current = value;
+                _ = Runner();
+            }
+        }
+
+        public void Add(Func<ValueTask> func)
+        {
+            if(current == null)
+            {
+                Current = func;
+            }
+            else
+            {
+                queued = func;
+            }
+        }
+    }
+}
